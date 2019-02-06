6a1dcbf764b662c5ae5ea28ffa4d4aa536aaeca9
Ulysse COATES <u.coates@gmail.com>
2019-02-06 18:58:01 +0000

    added logo

--- src/components/layout/header/PageHeader.js
+++ src/components/layout/header/PageHeader.js
@@ -6,7 +6,12 @@ class PageHeader extends Component {
   render() {
     return (
       <header>
-        <NavBar/>
+        <div className="header__top">
+          <div className="logo_container">
+          <img className="logo" src="https://z-m-scontent.flis5-1.fna.fbcdn.net/v/t1.0-9/50481853_2500649853342915_5506782981307498496_n.jpg?_nc_cat=108&_nc_ad=z-m&_nc_cid=1272&_nc_zor=9&_nc_ht=z-m-scontent.flis5-1.fna&oh=7c00ad8687755a389dcec549346b4de1&oe=5CFAB789"/>
+          </div>
+          <NavBar/>
+        </div>
         <img className="header__banner" src="http://ucf.org.au/wp-content/uploads/ucf-gradient-banner.jpg" alt="Gradient banner from ucf.org.au"/>
       </header>
     )
